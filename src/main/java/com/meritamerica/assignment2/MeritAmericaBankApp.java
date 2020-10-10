033ea17c2e72a3847bb6daa844f004a065044226
AllanaG <allanagray10@gmail.com>
2020-10-10 13:43:18 -0500

    Revert "Updates"
    This reverts commit 849332aeb22d41a52ef95c6788307d39e5d6f37d.

---
+++ src/main/java/com/meritamerica/assignment2/MeritAmericaBankApp.java
@@ -0,0 +1,16 @@
+/* Week 3 Partner Pair Assignment #2
+ * Bank Account Part 2  
+ * @author Michelle Neptune
+ * @date October 10, 2020
+ * PART 2
+ */
+
+
+
+package com.meritamerica.assignment2;
+
+public class MeritAmericaBankApp {
+	public static void main(String[] args) {
+		System.out.println("Hello Merit America!");
+	}
+}
