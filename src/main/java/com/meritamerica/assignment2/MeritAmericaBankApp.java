aa7c153edd3b7a2460bd8dbae588c9aa0c784676
MichelleArcelia <michellea.neptune@gmail.com>
2020-10-12 15:03:07 -0500

    Set CD amounts in MeritBankApp

--- src/main/java/com/meritamerica/assignment2/MeritAmericaBankApp.java
+++ src/main/java/com/meritamerica/assignment2/MeritAmericaBankApp.java
@@ -14,21 +14,32 @@ public class MeritAmericaBankApp {
 	
 	public static void main(String[] args) {
 		
-	
-// *************** PROGRAM PRINT FILES ***************
-				
-
-//  ============= RUNNING TESTS ON ACCOUNT HOLDER ============= 	          
+      
 		        
 	System.out.println("******************************************");
 	System.out.println("   Welcome to Merit America Bank App Version 2!");
 	System.out.println("    Your account details are below:");
 	System.out.println("******************************************");
-		
+
+//Set CD Offerings
+	
+	CDOffering[] CDOfferings = new CDOffering[5];
+		CDOffering[0] = new CDOffering(1, .018);
+		CDOffering[1] = new CDOffering(2, .019);
+		CDOffering[2] = new CDOffering(3, .020);
+		CDOffering[3] = new CDOffering(5, .025);
+		CDOffering[4] = new CDOffering(10, .022);
 	
 	
+	MeritBank.setCDOfferings(CDOfferings);
 	
 	AccountHolder Holder1 = new AccountHolder("Mare", "the", "cat", "123-45-6789");
+	
+	
+	
+	
+	
+	
 	
 	System.out.println(Holder1.toString());	
 	
